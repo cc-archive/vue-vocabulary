7b19bf6740ce759990251bc2361702c8649285f5
Hamza Nouali <hamzanouali1998@gmail.com>
2019-10-04 20:19:11 +0200

    Unit Tests - ImageView.spec.js

---
+++ tests/unit/elements/ImageView.spec.js
@@ -0,0 +1,117 @@
+import { config, shallowMount } from '@vue/test-utils'
+import ImageView from '@/elements/ImageView/ImageView'
+
+describe('ImageView.vue', () => {
+  const SOURCE = 'source demo';
+  const ALTERNATE_TEXT = 'hello';
+  const TITLE = 'title demo';
+  const PRIMARY_DIMENSION_HEIGHT = 'height';
+  const PRIMARY_DIMENSION_WIDTH = 'width';
+  const IS_CENTERED = true;
+  const IS_HOVERABLE = true;
+  const SIZE = 'mega';
+  const SLOT = '<strong>hello world</strong>';
+
+  let defaultConfig = null;
+
+
+  beforeEach(() => {      
+    defaultConfig = {
+      propsData: {
+
+          source: SOURCE,
+          alternateText: ALTERNATE_TEXT,
+
+      }
+    };
+  })
+
+
+  function getWrapper() {
+    return shallowMount(ImageView, defaultConfig)
+  }
+  
+  it ('renders the correct image for the given source', () => {
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('img[src="'+ SOURCE +'"]').exists()).toBe(true)
+  })
+
+  it ('renders the correct alternate text', () => {
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('img[alt="'+ ALTERNATE_TEXT +'"]').exists()).toBe(true)
+  })
+
+  it ('renders the correct title', () => {
+    defaultConfig.propsData.title = TITLE;
+    defaultConfig.propsData.isHoverable = IS_HOVERABLE;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find('.addons.bottom').text()).toContain(TITLE)
+  })
+
+  it ('renders the correct height dimension constraint', () => {
+    defaultConfig.propsData.primaryDimension = PRIMARY_DIMENSION_HEIGHT;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.${PRIMARY_DIMENSION_HEIGHT}-constrained`).exists()).toBe(true)
+  })
+
+  it ('renders the correct width dimension constraint', () => {
+    defaultConfig.propsData.primaryDimension = PRIMARY_DIMENSION_WIDTH;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.${PRIMARY_DIMENSION_WIDTH}-constrained`).exists()).toBe(true)
+  })
+  
+  it ('centering image is working correctly', () => {
+    defaultConfig.propsData.isCentered = IS_CENTERED;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.centered`).exists()).toBe(true)
+  })
+
+  it ('hovering image is working correctly', () => {
+    defaultConfig.propsData.isHoverable = IS_HOVERABLE;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.hoverable`).exists()).toBe(true)
+  })
+
+  it ('rendering the correct size', () => {
+    defaultConfig.propsData.size = SIZE;
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.${SIZE}-sized`).exists()).toBe(true)
+  })
+
+  it ('rendering the topAddons correctly', () => {
+    defaultConfig.propsData.isHoverable = IS_HOVERABLE;
+    defaultConfig.slots = {
+        topAddons: SLOT
+    };
+
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.addons.top`).html()).toContain(SLOT)
+  })
+
+  it ('rendering the bottomAddons correctly', () => {
+    defaultConfig.propsData.isHoverable = IS_HOVERABLE;
+    defaultConfig.slots = {
+        bottomAddons: SLOT
+    };
+
+    const wrapper = getWrapper();
+
+    expect(wrapper.find(`.addons.bottom`).html()).toContain(SLOT)
+  })
+
+});
+
+
+
+
+
+
