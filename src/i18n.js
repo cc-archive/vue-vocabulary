887cc4faf3499bfc27fac8b255937ed4307477f9
Sa'id Bello <50219753+Pilgrim001@users.noreply.github.com>
2019-10-06 09:46:26 +0100

    Moving changes to the Locale component

--- src/i18n.js
+++ src/i18n.js
@@ -16,9 +16,8 @@ function loadLocaleMessages () {
   return messages
 }
 
-export default new VueI18n({
-  // locale checks if language exists in local storage, if yes it gets language or uses what is next in the OR statement 
-  locale: localStorage.getItem('locale') || process.env.VUE_APP_I18N_LOCALE || 'en',
+export default new VueI18n({ 
+  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages()
 })
