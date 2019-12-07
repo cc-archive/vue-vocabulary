b3b18085c5353051c69ab0419301acb038d8d01a
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2019-12-07 02:01:46 +0100

    Adds twemoji integration

---
+++ src/elements/Twemoji/Twemoji.vue
@@ -0,0 +1,17 @@
+<template>
+  <div ref="emoji">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import twemoji from 'twemoji'
+  export default {
+    name: 'Twemoji',
+    mounted () {
+      twemoji.parse(this.$refs.emoji, this.option)
+    }
+  }
+</script>
+
+<style lang="stylus" src="./Twemoji.styl"></style>
