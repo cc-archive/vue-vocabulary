f8377c60f081bd7c3f6d5cf818f0ed61957264f6
Chidiebere <chidex5@gmail.com>
2019-12-14 22:03:38 +0100

    Fix color styling and added opacity property to color

--- src/mixins/colored.js
+++ src/mixins/colored.js
@@ -10,17 +10,18 @@ export default {
      */
     color: {
       type: String,
-      validator: val => [
-        'blue',
-        'green',
-        'magenta',
-        'olive',
-        'orange',
-        'purple',
-        'red',
-        'sand',
-        'yellow'
-      ].includes(val)
+      validator: val =>
+        [
+          'blue',
+          'green',
+          'magenta',
+          'olive',
+          'orange',
+          'purple',
+          'red',
+          'sand',
+          'yellow'
+        ].includes(val)
     },
     /**
      * _the accentuating shade of the primary color to use_
@@ -31,11 +32,17 @@ export default {
      */
     shade: {
       type: String,
-      validator: val => [
-        'light',
-        'dark',
-        'darker'
-      ].includes(val)
+      validator: val => ['light', 'dark', 'darker'].includes(val)
+    },
+    /**
+     * _the accentuating opacity of the primary color to use from 0 to 9_
+     *
+     * If the opacity is not specified, the base color with opacity of 9 will be used.
+     */
+    opacity: {
+      type: Number,
+      default: 9,
+      validator: val => val >= 0 && val <= 9
     }
   },
   computed: {
@@ -43,6 +50,9 @@ export default {
       let classes = []
       if (this.color) {
         classes.push(`${this.color}-colored`)
+      }
+      if (this.opacity) {
+        classes.push(`${this.opacity}-opacity`)
       }
       if (this.shade) {
         classes.push(`${this.shade}-shaded`)
