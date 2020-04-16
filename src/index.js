4d03029e37cbd91b4bb33ff83c3586e47089db4e
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-16 21:33:43 +0530

    Fix the indexing process

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@
 
 import SlotRenderer from './utils/SlotRenderer/SlotRenderer'
 
-import Tab from './layouts/Tab/Tab'
-import Tabs from './layouts/Tab/Tabs'
+import Tabs from './layouts/Tabs/Tabs'
+import Tab from './layouts/Tabs/Tab'
 import Table from './layouts/Table/Table'
 import TableCell from './layouts/Table/TableCell'
 
@@ -20,8 +20,8 @@ import Locale from './patterns/Locale/Locale'
 export {
   SlotRenderer,
 
-  Tab,
   Tabs,
+  Tab,
   Table,
   TableCell,
 
@@ -35,8 +35,8 @@ export default {
   install: function (Vue) {
     Vue.component('SlotRenderer', SlotRenderer)
 
-    Vue.component('Tab', Tab)
     Vue.component('Tabs', Tabs)
+    Vue.component('Tab', Tab)
     Vue.component('Table', Table)
     Vue.component('TableCell', TableCell)
 
