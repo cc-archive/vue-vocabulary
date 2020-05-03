02b257b634213c3e0f073f77eb92c30e974abda6
Jahnvi <jahnvigupta11@gmail.com>
2020-05-03 09:43:38 +0530

    Fixed xMidyMeeet

--- src/patterns/Header/Header.vue
+++ src/patterns/Header/Header.vue
@@ -6,7 +6,7 @@
             <svg
               class="logo"
               xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidyMid meet"
+              preserveAspectRatio="xMidYMid meet"
               viewBox="0 0 304 73">
               <use :href="`${svg}#logomark`"></use>
             </svg>
